7444a14ed51ef5e78a96bbcf4ec1aca97905c311
aykutakin <aykutakin90@gmail.com>
2013-07-03 13:58:49 +0300

    Sample Spring Project Structure

---
+++ src/main/java/com/mydomain/controller/LoginController.java
@@ -0,0 +1,15 @@
+package com.mydomain.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/")
+	public String login() {
+		return "redirect:/index.jsp";
+	}
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Index</title>
+    <link href="css/bootstrap.css" rel="stylesheet" media="screen">
+</head>
+<body>
+    <h2>Hello World!</h2>
+    <input type="text" placeholder="Enter a name here">
+    <script src="js/bootstrap.js"></script>
+</body>
+</html>
